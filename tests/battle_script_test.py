bc9caeaa3b26540b8d7570b70ddb845fb5ebe818
MainPC <apxujiy3@gmail.com>
2022-11-30 23:53:40 +0300

    added skeleton for test functions

--- tests/battle_script_test.py
+++ tests/battle_script_test.py
@@ -1,28 +1,55 @@
 import BattleScript.main as script
-import openpyxl as op
 import random
-import pathlib
 
 
-path = pathlib.Path(pathlib.Path.home(), "PycharmProjects", "battle_script_for_roleplay_game", 'BattleScript',
-                    "default_army.xlsx")
-xls_workbook = op.load_workbook(path, read_only=True)
-xls_worksheet = xls_workbook.active
+def test_common_battle():
+    pass
 
 
-class ParserTest(TestCase):
-    def test_type(self):
-        bbb = army_creator(10, "parsing")
-        ttt = script.army_list_creator(bbb)
+def test_wipe_single_unit():
+    pass
 
 
-    def test_number_of_units_after_parsing(self):
-        self.assertNotEqual(script.army_list_creator(army_creator(random.randint(1, 10), "parsing")),
-                            army_creator(random.randint(1, 10), "first-stage"))
+def test_massive_battle():
+    pass
 
-    def test_army_without_unit_renaming(self):
-        list_for_testing = [{"unit_name": "название_юнита", "unit_type": "в", "max_combat": 100, "current_combat": 75,
-                             "damage": 15, "defence": 10, "morale": 10, "targets": "в,л,о", "amount": 2}]
-        bad_result = [{"unit_name": "название_юнита", "unit_type": "в", "max_combat": 100, "current_combat": 75,
-                       "damage": 15, "defence": 10, "morale": 10, "targets": "в,л,о"}] * 2
-        self.assertNotEqual(script.army_list_creator(list_for_testing), bad_result)
+
+def test_damage_in_first_stage():
+    pass
+
+
+def test_parser_class():
+    pass
+
+
+def test_first_battle_stage_class():
+    pass
+
+
+def test_second_battle_stage_class():
+    pass
+
+
+def test_remove_dead_units_function():
+    pass
+
+
+def test_count_shartage_before_and_after_battle_function():
+    pass
+
+
+def test_remove_negative_casualties_from_graveyard_function():
+    pass
+
+
+def test_write_txt_file_function():
+    pass
+
+
+def test_write_army_statistics_function():
+    pass
+
+
+def test_main_logic_function():
+    pass
+
